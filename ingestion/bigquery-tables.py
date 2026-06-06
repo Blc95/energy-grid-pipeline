eee3e9972eeefbbfddb926fcd534116f50e01ead
Benjamin Colding <benjamincolding@gmail.com>
2026-06-06 14:32:39 +0200

    updated bigquery and ingestion to accomondate more tables and zones

--- ingestion/bigquery-tables.py
+++ ingestion/bigquery-tables.py
@@ -3,22 +3,45 @@ from google.cloud import bigquery
 client = bigquery.Client(project="energy-grid-pipeline")
 
 # Creating the table
-table_id = "energy-grid-pipeline.raw.prices"
-schema = [
+table_id_prices = "energy-grid-pipeline.raw.prices"
+schema_prices = [
     bigquery.SchemaField("timestamp", "TIMESTAMP"),
+    bigquery.SchemaField("delivery_date", "DATE"),
     bigquery.SchemaField("price", "FLOAT64"),
     bigquery.SchemaField("zone", "STRING"),
 ]
 
-table = bigquery.Table(table_id, schema=schema)
-table.time_partitioning = bigquery.TimePartitioning(
+table_prices = bigquery.Table(table_id_prices, schema=schema_prices)
+table_prices.time_partitioning = bigquery.TimePartitioning(
     type_=bigquery.TimePartitioningType.DAY,
-    field="timestamp",
+    field="delivery_date",
 )
 
-table = client.create_table(table, exists_ok=True)
+table_prices = client.create_table(table_prices, exists_ok=True)
 
 print(
-    f"Created table {table.project}.{table.dataset_id}.{table.table_id}, "
-    f"partitioned on column {table.time_partitioning.field}."
+    f"Created table {table_prices.project}.{table_prices.dataset_id}.{table_prices.table_id}, "
+    f"partitioned on column {table_prices.time_partitioning.field}."
+)
+
+table_id_load = "energy-grid-pipeline.raw.load"
+
+schema_load = [
+    bigquery.SchemaField("timestamp", "TIMESTAMP"),
+    bigquery.SchemaField("delivery_date", "DATE"),
+    bigquery.SchemaField("actual_load_mw", "FLOAT64"),
+    bigquery.SchemaField("zone", "STRING"),
+]
+
+table_load = bigquery.Table(table_id_load, schema=schema_load)
+table_load.time_partitioning = bigquery.TimePartitioning(
+    type_=bigquery.TimePartitioningType.DAY,
+    field="delivery_date",
+)
+
+table_load = client.create_table(table_load, exists_ok=True)
+
+print(
+    f"Created table {table_load.project}.{table_load.dataset_id}.{table_load.table_id}, "
+    f"partitioned on column {table_load.time_partitioning.field}."
 )
--- ingestion/ingestion.py
+++ ingestion/ingestion.py
@@ -17,93 +17,127 @@ env_path = Path(__file__).resolve().parent.parent / ".env"
 
 load_dotenv(dotenv_path=env_path)
 
-
-def build_gcs_path(country_code, date):
-    return f"gs://energy-grid-pipeline-raw/prices/{country_code}/{date}/data.parquet"
-    
+def build_gcs_path(country_code, date, table):
+    return f"gs://energy-grid-pipeline-raw/{table}/{country_code}/{date}/data.parquet"
 
 
-
-def fetch_data(country_code, start, end):
+def fetch(country_code, start, end, query_method, value_column, bucket):
     token = os.getenv("ENTSOE_TOKEN")
-    
     if not token:
         raise ValueError("Error: ENTSOE_TOKEN not set in environment")
     
     client = EntsoePandasClient(api_key=token)
     
-    data = client.query_day_ahead_prices(country_code=country_code,
-                                         start=start,
-                                         end=end
-                                         )
+    method = getattr(client, query_method)
+    data = method(
+        country_code=country_code,
+        start=start,
+        end=end
+    )
     
     df = data.reset_index()
-    df.columns = ["timestamp", "price"]
+    df.columns = ["timestamp", value_column]
     df["zone"] = country_code
     df = df[df["timestamp"] < end]
-    # Align with BigQuery's UTC-based DAY partitioning so each file maps to one partition.
+    
+    df["delivery_date"] = df["timestamp"].dt.tz_convert("Europe/Copenhagen").dt.date
     df["timestamp"] = df["timestamp"].dt.tz_convert("UTC")
-    days = df.groupby(pd.Grouper(key='timestamp', freq='D'))
+    
+    days = df.groupby("delivery_date")
     
     for day, group in days:
-        group = group[group["timestamp"].dt.normalize() == day]
         if group.empty:
             continue
         day_str = day.strftime('%Y-%m-%d')
-        gcs_path = build_gcs_path(country_code=country_code, date=day_str)
+        gcs_path = build_gcs_path(country_code=country_code, date=day_str, table=bucket)
         group.to_parquet(gcs_path)
      
      
-        
-def load_to_big_query(date, country_code):
+def load_to_big_query(date, country_code, table):
     client = bigquery.Client(project="energy-grid-pipeline")
     
+    # For DELETE QUERY
+    table_id_base = f"energy-grid-pipeline.raw.{table}" 
+    date_for_query = date.strftime(("%Y-%m-%d"))
+    
+    query = f"""
+        DELETE FROM `{table_id_base}`
+        WHERE zone = @zone 
+        AND delivery_date = @delivery_date
+    """
+
+    job_config = bigquery.QueryJobConfig(
+        query_parameters=[
+            bigquery.ScalarQueryParameter("zone", "STRING", country_code),
+            bigquery.ScalarQueryParameter("delivery_date", "DATE", date_for_query),
+        ]
+    )
+
+    job = client.query(query, job_config=job_config)
+    job.result()
+    
     job_config = bigquery.LoadJobConfig(
-    write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+    write_disposition=bigquery.WriteDisposition.WRITE_APPEND,
     source_format=bigquery.SourceFormat.PARQUET,
     time_partitioning=bigquery.TimePartitioning(
         type_=bigquery.TimePartitioningType.DAY,
-        field="timestamp"
+        field="delivery_date"
         )
     )
     
-    date_nodashes = date.strftime("%Y%m%d")
-    table_id = f"energy-grid-pipeline.raw.prices${date_nodashes}"
+    uri = build_gcs_path(country_code=country_code,
+                         date=date.strftime("%Y-%m-%d"),
+                         table=table)
     
-    uri = build_gcs_path(country_code=country_code, date=date.strftime("%Y-%m-%d"))
     
     load_job = client.load_table_from_uri(
-    uri, table_id, job_config=job_config
+    uri, table_id_base, job_config=job_config
     )  # Make an API request.
 
     load_job.result()  # Waits for the job to complete.
 
-    destination_table = client.get_table(table_id)
+    destination_table = client.get_table(table_id_base)
     print("Loaded {} rows.".format(destination_table.num_rows))
 
 def main():
-    country_code = "DK_1"
-    start = pd.Timestamp('20260520', tz='Europe/Brussels')
-    end = pd.Timestamp('20260525', tz='Europe/Brussels')
+    country_codes = ["DK_1", "DK_2"]
+    start = pd.Timestamp('20251025', tz='Europe/Brussels')
+    end = pd.Timestamp('20251027', tz='Europe/Brussels')
+    tables = ["prices", "load"]
+    query_methods = ["query_day_ahead_prices", "query_load"]
     
-    fetch_data(
-        country_code=country_code,
-        start=start,
-        end=end
-    ) 
+    # for query_method in query_methods:
+    #     if query_method == "query_day_ahead_prices":
+    #         bucket = "prices"
+    #         value_column = "price"
+    #     elif query_method == "query_load":
+    #         bucket = "load"
+    #         value_column = "actual_load_mw"
+            
+    #     for country_code in country_codes:
+    #         fetch(country_code=country_code,
+    #             start=start,
+    #             end=end,
+    #             query_method=query_method,
+    #             value_column=value_column,
+    #             bucket=bucket)
     
     dates = pd.date_range(
-        start=start.tz_convert("UTC").normalize(),
-        end=end.tz_convert("UTC").normalize(),
+        start=start,
+        end=end,
         freq='D',
     )[:-1]
-    
-    for day in dates:
-        print(day, day.strftime("%Y%m%d"))
-        load_to_big_query(
-            country_code=country_code,
-            date=day
-        )
+
+    for table in tables:
+        for country_code in country_codes:
+            for day in dates:
+                print(day, day.strftime("%Y%m%d"))
+                print(country_code)
+                load_to_big_query(
+                    country_code=country_code,
+                    date=day,
+                    table=table
+                )
 
 if __name__ == "__main__":
     main()
